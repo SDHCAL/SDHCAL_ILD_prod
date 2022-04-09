e6a535a589899faab6a60df19e196f74ade69966
Gerald Grenier <grenier@ipnl.in2p3.fr>
2022-04-09 15:55:05 +0200

    Add dirac script to perform Marlin Reco

--- DiracScripts/CommonStep.py
+++ DiracScripts/CommonStep.py
@@ -16,3 +16,25 @@ def detectorName(detector):
     if detector=="ILD_s2_v02":
         detector="SmallVideau"
     return detector
+
+def CalibDetectorName(detectorName):
+    calib=detectorName
+    if detectorName=="ILD_l2_v02":
+        calib="ILD_l5_o2_v02"
+    if  detectorName=="ILD_s2_v02":
+        calib="ILD_s5_o2_v02"
+    return calib
+
+def extraCLIargMarlinSDHCALRecoBase(jsdata):
+    extraCLIarg=""
+    #Fix to missing file in current ILDConfig when running Videau geometry
+    calibFile=jsdata['Detector']['CalibDetectorName']
+    if calibFile=="automatic":
+        calibFile=CalibDetectorName(jsdata['Detector']['DetectorName'])
+    extraCLIarg+=" --constant.CalibrationFile=Calibration/Calibration_{0}.xml".format(calibFile)
+    calib=jsdata['SDHCAL_Calibration']
+    extraCLIarg+=' --constant.SDHcalEnergyFactors="{0}"  --constant.PandoraEcalToHadBarrelScale="{1}" --constant.PandoraEcalToHadEndcapScale="{2}" --constant.PandoraHcalToHadScale="{3}"'.format(calib['SDHCALEnergyFactors'],calib['PandoraEcalToHadronicEnergyConversionBarrel'],calib['PandoraEcalToHadronicEnergyConversionEndcap'],calib['PandoraHcalToHadronicEnergyConversion'])
+    extraCLIarg+=' --constant.PandoraSettingsFile=PandoraSettings/PandoraSettingsPerfectPFA.xml'
+    #turn on calibration details in PfoAnalysis (requires full SimCalorimeterHitInfo)
+    extraCLIarg+=' --MyPfoAnalysis.CollectCalibrationDetails=1'
+    return extraCLIarg
---
+++ DiracScripts/SDHCAL_dirac_marlin_baseReco.py
@@ -0,0 +1,124 @@
+import sys
+import os
+import json
+import CommonStep as CS
+
+#Magic DIRAC needed voodoo
+from DIRAC.Core.Base import Script
+Script.parseCommandLine()
+
+from ILCDIRAC.Interfaces.API.DiracILC import DiracILC
+from ILCDIRAC.Interfaces.API.NewInterface.UserJob import  UserJob
+from ILCDIRAC.Interfaces.API.NewInterface.Applications import Marlin
+
+def myRecoJob(jsdata,dirac,inputFileNames,NeventsPerFile=0,NeventsPerJob=0):
+    """Create reco job
+    :param jsdata :: json job parameters 
+    :param inputFileNames :: list of string with full filenames in Dirac File Catalog or single string with the filename
+    :param int NeventsPerFile :: number of events in the file to process (0 means all)
+    :param int NeventsPerJob :: number of events to process in one job, one input file is splitted across many jobs. If set to something else than 0, NeventsPerFile should be set to non zero value.
+    When using NeventsPerFile, user should check the total number of events in his files to make sure  all events will be processed
+    """
+    fileList=inputFileNames  if isinstance(inputFileNames, list) else [inputFileNames]
+    if NeventsPerJob>0 and NeventsPerFile==0:
+        raise ValueError("Parameter NeventsPerFile should not be zero when NeventsPerJob is set to a non zero value.")
+
+    #definition of the job and some of its properties
+    job = UserJob()
+    #job names
+    job.setName( "MarlinStandardReco" )
+    job.setJobGroup( "Reco_base" )
+    #what to retrieve in the job output
+    job.setOutputSandbox(['*.log', '*.xml', '*.steer'])
+    #CPU time for the job 
+    job.setCPUTime(100 * 4 * 60)
+    #exclude some site (like failing site)
+    job.setBannedSites(['LCG.RAL-LCG2.uk'])
+    
+    job.setILDConfig(jsdata['SoftwareVersions']['ILDConfigVersion'])
+    job.setInputSandbox(["steeringFiles/MarlinStdRecoSDHCALExtendedDST.xml"])
+    singleJob=False
+    if NeventsPerJob>0:
+        job.setSplitFilesAcrossJobs(fileList, NeventsPerFile, NeventsPerJob)
+    else:
+        if len(fileList) > 1:
+            job.setSplitInputData(fileList)
+        else:
+            singleJob=True
+    base_output_name="reco_"+os.path.splitext(os.path.basename(fileList[0]))[0]
+    #remove eventual file job similation number assuming list of identical files is given
+    if not singleJob:
+        base_output_name=base_output_name.rsplit('_',1)[0]
+        if NeventsPerFile>0:
+            base_output_name+="_reco_{0}".format(NeventsPerFile)
+    if NeventsPerJob>0:
+        base_output_name+="_reco_{0}".format(NeventsPerFile/NeventsPerJob)
+    
+    #Marlin application
+    #First standard reco with Pandora Truth
+    ma = Marlin()
+    ma.setVersion(jsdata['SoftwareVersions']['marlinVersion'])
+    if singleJob:
+        ma.setInputFile(fileList[0])
+        if NeventsPerFile !=0:
+            ma.setNumberOfEvents(NeventsPerFile)
+    ma.setSteeringFile("MarlinStdReco.xml")
+    ma.setLogFile("marlin.log")
+    ma.setDetectorModel(jsdata['Detector']['DetectorName'])
+    extraCLIarg=CS.extraCLIargMarlinSDHCALRecoBase(jsdata)
+    extraCLIarg+=" --constant.OutputBaseName={0}".format(base_output_name)
+    ma.setExtraCLIArguments( extraCLIarg )
+
+    #CMSEnergy hould correspond to the existence of Config/Parameters${CMSEnergy}GeV.xml in ILDConfig
+    ma.setEnergy(jsdata['Detector']['CMSEnergy'])
+    outputRECfile="{0}_REC.slcio".format(base_output_name)
+    outputDSTfile="{0}_DST.slcio".format(base_output_name)
+    PFOoutputFile="{0}_PfoAnalysis.root".format(base_output_name)
+    ma.setOutputRecFile(outputRECfile)
+    ma.setOutputDstFile(outputDSTfile)
+    ma.setOutputFile(outputRECfile)
+    #append marlin to the job
+    res = job.append(ma)
+    if not res['OK']:
+        print res['Not ok appending Marlin to job']
+        quit()
+
+    #Second Marlin run to produce extended DST file
+    # NB : specific marlin steering file to be send in the job input sandbox
+    mabis = Marlin()
+    mabis.setVersion(jsdata['SoftwareVersions']['marlinVersion'])
+    mabis.getInputFromApp( ma )
+    mabis.setSteeringFile("MarlinStdRecoSDHCALExtendedDST.xml")
+    mabis.setLogFile("marlin_bis.log")
+    mabis.setDetectorModel(jsdata['Detector']['DetectorName'])
+    extraCLIarg=""
+    extraCLIarg+=" --constant.OutputBaseName={0}".format(base_output_name)
+    mabis.setExtraCLIArguments( extraCLIarg )
+    mabis.setEnergy(jsdata['Detector']['CMSEnergy'])
+    outputextDSTfile="{0}_extDST.slcio".format(base_output_name)
+    mabis.setOutputRecFile(outputextDSTfile)
+    mabis.setOutputFile(outputextDSTfile)
+    #append marlin to the job
+    res = job.append(mabis)
+    if not res['OK']:
+        print res['Not ok appending Marlin to job']
+        quit()
+
+    #tell the job where to put the output file on the grid
+    #first argument is the list of input file
+    #second is the grid directory under /ilc/user/u/username/ where the output will be copied
+    job.setOutputData([outputRECfile,outputextDSTfile,PFOoutputFile],jsdata['JobParameters']['output_dir'],OutputSE=['DESY-SRM','CERN-DST-EOS','IN2P3-SRM'])
+
+    #submit the job 
+    print job.submit(dirac)
+    
+if __name__ == '__main__':
+    #manageParameters
+    fp=open("json/SDHCAL_baseReco.json")
+    js=json.load(fp)
+    #create the object that will managed the job submission
+    dirac = DiracILC(True,"reco.rep")
+    #submit the jobs
+    JobParams=js['JobParameters']
+    myRecoJob(js,dirac,JobParams['InputFiles'],NeventsPerFile=JobParams['NumberOfEventsToProcessInFilePerJob'],NeventsPerJob=JobParams['NumberOfJobsPerFileIfSPlitFile'])
+    
